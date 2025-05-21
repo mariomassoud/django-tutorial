1a98d0ab56352c0605e5176920b172b47584b59e
Mario Massoud <mario.massoud@lau.edu>
2025-05-21 17:07:11 +0300

    Django tutorial part 3 done

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
   path("",views.index,name="index"),
   path("about/",views.about,name="about"),
+  path("<int:question_id>/",views.detail,name="detail"),
+  path("<int:question_id>/results/",views.results,name="result"),
+  path("<int:question_id>/vote/",views.vote,name="vote"),
 ]
